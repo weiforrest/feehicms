99f42402b598b8a5e41ab16e2eac20eacc310953
weiforrest <weiforrest@gmail.com>
2019-04-02 22:18:20 +0800

    优化值班后台添加操作

--- backend/controllers/DutyController.php
+++ backend/controllers/DutyController.php
@@ -19,9 +19,9 @@ class DutyController extends \yii\web\Controller
     /**
      * @auth
      * - item group=运营管理 category=值班 description=列表 sort=800 method=get
-     * - item group=运营管理 category=值班 description=创建 sort=801 method=get
-     * - item group=运营管理 category=值班 description=修改 sort=802 method=get
-     * - item group=运营管理 category=值班 description=删除 sort=803 method=get
+     * - item group=运营管理 category=值班 description=创建 sort=801 method=get,post
+     * - item group=运营管理 category=值班 description=修改 sort=802 method=get,post
+     * - item group=运营管理 category=值班 description=删除 sort=803 method=get,post
      * - item group=运营管理 category=值班 description=排序 sort=804 method=get
      * - item group=运营管理 category=值班 description=查看 sort=805 method=get
 * 
@@ -35,7 +35,7 @@ class DutyController extends \yii\web\Controller
                 'data' => function(){
                     
                         $dataProvider = new ActiveDataProvider([
-                            'query' => Duty::find(),
+                            'query' => Duty::find()->orderBy('duty_time desc'),
                         ]);
 
                         return [
--- backend/views/duty/create.php
+++ backend/views/duty/create.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Url;
+use common\models\Duty;
 
 
 /* @var $this yii\web\View */
@@ -10,6 +11,9 @@ $this->params['breadcrumbs'] = [
     ['label' => yii::t('app', 'Duty'), 'url' => Url::to(['index'])],
     ['label' => yii::t('app', 'Create') . yii::t('app', 'Duty')],
 ];
+$model=Duty::find()->limit(1)->orderBy('duty_time desc')->one();
+$model->duty_time = date('Y-m-d',strtotime("$model->duty_time +1 day"));
+$model->gun ='';
 ?>
 <?= $this->render('_form', [
     'model' => $model,
